d71f237e15e971d383a114c3cca274532de76d14
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-09-11 03:10:59 +0800

    feat: Add react-geolocated dependency and implement geolocation feature in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useGeolocated } from "react-geolocated";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return !isGeolocationAvailable ? (
+    <div>Your browser does not support Geolocation</div>
+  ) : !isGeolocationEnabled ? (
+    <div>Geolocation is not enabled</div>
+  ) : coords ? (
+    <table>
+      <tbody>
+        <tr>
+          <td>latitude</td>
+          <td>{coords.latitude}</td>
+        </tr>
+        <tr>
+          <td>longitude</td>
+          <td>{coords.longitude}</td>
+        </tr>
+        <tr>
+          <td>altitude</td>
+          <td>{coords.altitude}</td>
+        </tr>
+        <tr>
+          <td>heading</td>
+          <td>{coords.heading}</td>
+        </tr>
+        <tr>
+          <td>speed</td>
+          <td>{coords.speed}</td>
+        </tr>
+      </tbody>
+    </table>
+  ) : (
+    <div>Getting the location data&hellip; </div>
+  );
 }
 
-export default App
+export default App;
